b81a7a6e4494dde4fc9ed0cb2a71c0b82ed88554
HELPSYSTEMS\agasi.bekiryan <agasi.bekiryan@helpsystems.com>
2024-05-24 20:16:01 +0400

    Added get info endpoint data, added docstrings and connected server to model

--- src/model/main.py
+++ src/model/main.py
@@ -1,9 +1,11 @@
 from src.model.data_processing import load_data
 from src.model.model import NaiveBayes
 from src.model.utils import split_data
+import os
 
 # Load train_data
-df = load_data('train_data/spam.csv')
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+df = load_data(f'{ROOT_DIR}/train_data/spam.csv')
 
 # Split train_data
 X_train, X_test, y_train, y_test = split_data(df)
--- src/server/main.py
+++ src/server/main.py
@@ -1,19 +1,40 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from models import Message
+from src.model.main import nb
 
 app = FastAPI()
 
 
 @app.get("/info")
 async def info():
-    return {"Hello": "World"}
+    """
+    **Information about the algorithm.**
+
+    * **Returns:** algorithm name, version, status, status code, message, and error.
+    """
+    return {"algorithm_name": "Naive Bayes",
+            "version": "0.0.1",
+            "status": "up",
+            "status_code": 200,
+            "message": "OK",
+            "error": ""}
 
 
 @app.post("/predict")
 async def predict(message: Message):
-    if message.text == "Hello":
-        return {"prediction": "ham"}
-    return {"prediction": "spam"}
+    """
+    **Predicts whether a message is spam or ham.**
+
+    * **Parameter:** message to classify.
+    * **Returns:** prediction.
+    """
+    if message.text:
+        prediction = nb.predict([message.text])
+        if prediction[0] == 0:
+            return {"prediction": "ham"}
+        return {"prediction": "spam"}
+
+    raise HTTPException(status_code=400, detail="Message should not be empty")
 
 
 if __name__ == "__main__":
